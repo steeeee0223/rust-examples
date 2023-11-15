5958a84b24d2a405c8018d563d576ea6569a96ff
steeeee0223 <mr.steven7@gmail.com>
2023-11-15 14:05:44 +0000

    🔧 Fix examples in `exceptions`

--- exceptions/examples/ex-07.rs
+++ exceptions/examples/ex-07.rs
@@ -1,15 +1,15 @@
 fn main() {
     println!("🎯 Exceptions - Practice");
 
-    let res: Result<u32, &str> = divide(3, 2);
+    let res: Result<u32, String> = divide(3, 2);
     println!("💡 3 / 2 = {:?}", res.unwrap());
-    let res: Result<u32, &str> = divide(3, 0);
+    let res: Result<u32, String> = divide(3, 0);
     println!("💡 3 / 0 = {:?}", res);
 }
 
-fn divide(x: u32, y: u32) -> Result<u32, &'static str> {
+fn divide(x: u32, y: u32) -> Result<u32, String> {
     if y == 0 {
-        Err("Zero divisor error")
+        Err(String::from("Zero divisor error"))
     } else {
         Ok(x / y)
     }
